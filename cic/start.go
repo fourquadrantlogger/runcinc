91ff4dc9f8bbff40202f52d464a94ab51af8e3a0
lipeng <734991033@qq.com>
2021-09-02 15:09:23 +0800

    merge env

--- cic/start.go
+++ cic/start.go
@@ -2,9 +2,20 @@ package cic
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
 	"runcic/cic/fs"
+	"runcic/utils"
 )
 
+func (r *Runcic) SetEnv(copyParentEnv bool) {
+	if copyParentEnv {
+		r.Envs = append(r.Envs, os.Environ()...)
+	}
+	os.Clearenv()
+	for k, v := range utils.ParseEnvs(r.Envs) {
+		os.Setenv(k, v)
+	}
+}
 func (r *Runcic) Start() (err error) {
 	if err = r.cicvolume(); err != nil {
 		return
@@ -27,8 +38,9 @@ func (r *Runcic) Start() (err error) {
 	if err = fs.Link(); err != nil {
 		logrus.Errorf("fs link failed %s", err.Error())
 	}
-
+	r.SetEnv(false)
 	logrus.Infof("%+v %+v", r.Command, r.Envs)
+
 	err = Execv(r.Command[0], r.Command[1:], r.Envs)
 	if err != nil {
 		logrus.Errorf("exec exited %v", err.Error())
