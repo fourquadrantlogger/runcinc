135f86d4703e43eb27403e41d41df80b24858dbe
lipeng <734991033@qq.com>
2021-09-03 16:40:51 +0800

    重写flag解析

--- runcic.sh
+++ runcic.sh
@@ -1,6 +1,6 @@
 name=$1
 cicroot=/runcic/overlay/${name}
-runcic --name $name ${@:2}
+runcic runin --name=$name ${@:3}
 umount ${cicroot}/dev/mqueue
 umount ${cicroot}/dev/shm
 umount ${cicroot}/dev/pts
