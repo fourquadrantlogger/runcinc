ba57b23286e84e70cd21010a7b475259fafae3ab
lipeng <734991033@qq.com>
2021-09-10 16:13:26 +0800

    JSONFormatter

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"runcic/cmd"
 )
 
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		PrettyPrint:     true,
+	})
+}
 func main() {
 	cmd.Execute()
 }
