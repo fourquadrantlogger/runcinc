490266e1fda89b512fc0d02f5889363a10e5db66
lipeng <734991033@qq.com>
2021-09-14 16:25:03 +0800

    umount -A  --recursive  ${cicroot}

--- runcic.sh
+++ runcic.sh
@@ -1,12 +1,4 @@
 name=$1
 cicroot=/runcic/overlay/${name}
 runcic runin --name=$name ${@:3}
-umount ${cicroot}/dev/mqueue
-umount ${cicroot}/dev/shm
-umount ${cicroot}/dev/pts
-umount ${cicroot}/dev
-umount ${cicroot}/proc
-umount ${cicroot}/sys
-umount ${cicroot}/.PlnPyKFp4CRfFtgC1_run
-set -e
-umount ${cicroot}
+umount -A  --recursive ${cicroot}
